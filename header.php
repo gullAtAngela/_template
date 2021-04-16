f045c1bca50b10239b8729a0312087e2b2618d3d
Thomas Gull <thomas@gulldesign.ch>
2021-04-16 15:39:44 +0200

    bugfix path link

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
 	<div class="row mb-3">
 		<div class="col-sm-12 col-md-3">
-			<a href="/angela/helpportal"><img src="http://intern.angela.ch/ecommerce/_template/img/logoangela.svg" alt="Angela Bruderer Logo"></a>
+			<a href="/ecommerce/angela/helpportal"><img src="http://intern.angela.ch/ecommerce/_template/img/logoangela.svg" alt="Angela Bruderer Logo"></a>
 		</div>
 	</div>
 </header>
