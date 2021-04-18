7ece8830e7781ac0355a3700ee5ff74bf22c4b93
Thomas Gull <thomas@gulldesign.ch>
2021-04-18 13:11:25 +0200

    complete Footer

--- footer.php
+++ footer.php
@@ -1,66 +1,73 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <!--footer starts from here-->
 <footer class="footer">
-<div class="container bottom_border">
-<div class="row">
-<div class=" col-sm-4 col-md col-sm-4  col-12 col">
-<h5 class="headin5_amrc col_white_amrc pt2">Kontakt</h5>
-<!--headin5_amrc-->
-<p class="mb10">Thomas Gull<br>
-<strong>eCommerce</strong></p>
-<p><i class="fa fa-location-arrow"></i> Pflanzschulstrasse 17<br>8400 Winterthur </p>
-<p><i class="fa fa-phone"></i>  058 455 41 51  </p>
-<p><i class="fa fa fa-envelope"></i> <a href="mailto:thomas.gull@angela-bruderer.ch">thomas.gull@angela-bruderer.ch</a>  </p>
+	<div class="container bottom_border">
+		<div class="row">
+			<div class=" col-sm-4 col-md col-sm-4  col-12 col">
+				<h5 class="headin5_amrc col_white_amrc pt2">Kontakt</h5>
+				<!--headin5_amrc-->
+				<p class="mb10">Thomas Gull<br>
+					<strong>eCommerce</strong>
+				</p>
+				<p><i class="fa fa-location-arrow"></i> Pflanzschulstrasse 17<br>8400 Winterthur </p>
+				<p><i class="fa fa-phone"></i> 058 455 41 51 </p>
+				<p><i class="fa fa fa-envelope"></i> <a href="mailto:thomas.gull@angela-bruderer.ch">thomas.gull@angela-bruderer.ch</a> </p>
 
 
-</div>
+			</div>
 
 
-<div class=" col-sm-4 col-md  col-6 col offset-md-4">
-<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
-<!--headin5_amrc-->
-<ul class="footer_ul_amrc">
-<li><a href="">Magento</a></li>
-<li><a href="">Jira Intern</a></li>
-<li><a href="">Jira Extern</a></li>
-<li><a href="">…</a></li>
-<li><a href="">…</a></li>
-<li><a href="">…</a></li>
-</ul>
-<!--footer_ul_amrc ends here-->
-</div>
+			<div class=" col-sm-4 col-md  col-6 col offset-md-4">
+				<h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+				<!--headin5_amrc-->
+				<ul class="footer_ul_amrc">
+					<li><a href="https://www.angela-bruderer.ch/abmageadm/">AB Magento</a></li>
+					<li><a href="https://www.praktikus.ch/cs2/">PRA Magento</a></li>
+					<li><a href="https://jira.angela.ch/">AB Jira Intern</a></li>
+					<li><a href="https://jira.dotsource.de/">dot.Source Jira</a></li>
+					<li><a href="https://suite6.emarsys.net/">Emarsys Suite</a></li>
+				</ul>
+				<!--footer_ul_amrc ends here-->
+			</div>
 
 
-<div class=" col-sm-4 col-md  col-12 col">
-<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
-<!--headin5_amrc ends here-->
+			<div class=" col-sm-4 col-md  col-12 col">
+				<h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+				<!--headin5_amrc ends here-->
 
-<ul class="footer_ul2_amrc">
-<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
-<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
-<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a><p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p></li>
-</ul>
-<!--footer_ul2_amrc ends here-->
-</div>
-</div>
-</div>
+				<ul class="footer_ul2_amrc">
+					<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+						<p>Das <a href="https://www.gulldesign.ch/code/public">miniPIM</a> ist ab sofort offiziell im Einsatz. Meega. Vielen Dank an euch! Ich feuer das. #miniPIM</p>
+					</li>
+					<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+						<p>Ich habe das miniPIM nun mit einem ersten RC für den öffentlichen Feldtest ausgerollt.</p>
+					</li>
+					<li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+						<p>Das miniPIM wurde mit viel Begeisterung angenommen. Ich feier das!</p>
+					</li>
+				</ul>
+				<!--footer_ul2_amrc ends here-->
+			</div>
+		</div>
+	</div>
 
 
-<div class="container">
-<ul class="foote_bottom_ul_amrc">
-<li><a href="#">Helpportal</a></li>
-<li><a href="#">Artikelmanagement</a></li>
-<li><a href="#">Variationskarten</a></li>
-<li><a href="#">Bulk Link</a></li>
-<li><a href="#">Taxonomy</a></li>
-</ul>
-<!--foote_bottom_ul_amrc ends here-->
-<p class="text-center">Copyright @2021 - <?= date('Y') ?> | Designed with <i class="fas fa-heart"></i> by <a href="http://www.gulldesign.ch">gullDeisgn.ch</a></p>
+	<div class="container">
+		<ul class="foote_bottom_ul_amrc">
+			<li><a href="../">Helpportal</a></li>
+			<li><a href="https://www.gulldesign.ch/code/public">miniPIM</a></li>
+			<li><a href="../varcode/">Variationskarten</a></li>
+			<li><a href="../bulk-link-open/">Multi-Artikel öffnen</a></li>
+			<li><a href="../deeplink/">Deeplink erstellen</a></li>
+		</ul>
+		<!--foote_bottom_ul_amrc ends here-->
+		<p class="text-center">Copyright @2021 - <?= date('Y') ?> | Designed with <i class="fas fa-heart"></i> by <a href="https://www.gulldesign.ch">gullDesign.ch</a></p>
 
-<!--social_footer_ul ends here-->
-</div>
+		<!--social_footer_ul ends here-->
+	</div>
 
 </footer>
 
 </body>
+
 </html>
